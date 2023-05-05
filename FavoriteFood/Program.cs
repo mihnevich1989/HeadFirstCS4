484367a39b91c6ab5ba630edfc45b51632cfd53c
Dzimitry Mikhnevich <mikhnevich.work@mail.ru>
2023-05-05 10:40:55 +0300

    dictionary favotireFoods

---
+++ FavoriteFood/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace FavoriteFood
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> favoriteFoods = new Dictionary<string, string>();
+            favoriteFoods["Alex"] = "hot dogs";
+            favoriteFoods["A'ja"] = "pizza";
+            favoriteFoods["Jules"] = "falafel";
+            favoriteFoods["Naima"] = "spaghetti";
+
+            string name;
+            while((name = Console.ReadLine()) != "")
+            {
+                if (favoriteFoods.ContainsKey(name))
+                    Console.WriteLine($"{name}'s favorite food is {favoriteFoods[name]}");
+                else
+                    Console.WriteLine($"I don't know {name}'s favorite food");
+            }   
+        }
+    }
+}
